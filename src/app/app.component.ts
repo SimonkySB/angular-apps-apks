b2f08d1f380624e616fe42300806ee2982c298ac
simonky <simonasb19@gmail.com>
2024-09-26 08:05:10 -0300

    apks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['../demo-styling.css']
 })
 export class AppComponent {
+
   title = 'angular-quickstart';
+
+  descargarApk() {
+    const fileUrl = 'assets/app-release.apk';
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'mi-aplicacion.apk';
+    link.click();
+  }
 }
